29c99b8c3e969f86c8875ee75e0c61675285cc5e
evanjin <evanjin8@gmail.com>
2019-03-29 14:59:10 +0900

    1. 시작하기(강의 소개, 요구사항 분석, 코드 스캐폴딩) 완료

---
+++ lecture-vue-trello/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    {{msg}}
+  </div>
+</template>
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+<style>
+</style>
