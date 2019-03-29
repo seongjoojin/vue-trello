a6002c55e2972dbb3ed7af93da76c18c211cbdcc
evanjin <evanjin8@gmail.com>
2019-03-29 17:25:58 +0900

    라우터 링크

---
+++ lecture-vue-trello/src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+    <router-link to="/">Home</router-link>
+    <router-link to="/login">Login</router-link>
+  </div>
+</template>
+<script>
+	export default {
+		name: "Navbar"
+	}
+</script>
+<style scoped>
+</style>
