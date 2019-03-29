76f0dc4fda9bab8bc397bdfd98738fc892eb69ac
evanjin <evanjin8@gmail.com>
2019-03-29 17:05:59 +0900

    라우터 뷰

--- lecture-vue-trello/src/App.vue
+++ lecture-vue-trello/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    code start!
+    <router-view></router-view>
   </div>
 </template>
 <script>
---
+++ lecture-vue-trello/src/components/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+    Page not found
+  </div>
+</template>
+
+<script>
+	export default {
+		name: "NotFound"
+	}
+</script>
+
+<style scoped>
+
+</style>
