fa08e6fee177dbe8514ecd99505ba96379e16cdd
evanjin <evanjin8@gmail.com>
2019-04-01 14:54:00 +0900

    중첩 라우트

---
+++ lecture-vue-trello/src/components/Card.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+    Card
+    <div>cid: {{ cid }}</div>
+  </div>
+</template>
+<script>
+	export default {
+		name: "Card",
+    data() {
+		  return {
+		    cid: 0
+      }
+    },
+    watch: {
+      '$route'() {
+        this.cid = this.$route.params.cid
+      }
+    },
+    created() {
+		  this.cid = this.$route.params.cid
+    }
+  }
+</script>
+<style scoped>
+</style>
