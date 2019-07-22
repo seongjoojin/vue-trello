e7c0dcab2af8c9bfedd0d17fc002ef06b7a83527
Seongjoo Jin <evanjin8@gmail.com>
2019-07-22 14:34:20 +0900

    보드 목록 조회 화면

--- lecture-vue-trello/src/components/Home.vue
+++ lecture-vue-trello/src/components/Home.vue
@@ -1,17 +1,21 @@
 <template>
   <div>
-    Home
-    <div>
-      Board List:
-      <div v-if="loading">Loading...</div>
-      <div v-else>
-        <div v-for="b in boards" :key="b.id">{{ b }}</div>
+    <div class="home-title">Personal Boards</div>
+    <div class="board-list" ref="boardList">
+      <div
+        class="board-item"
+        v-for="b in boards"
+        :key="b.id"
+        :data-bgcolor="b.bgColor"
+        ref="boardItem"
+      >
+        <router-link :to="`/b/${b.id}`">
+          <div class="board-item-title">{{b.title}}</div>
+        </router-link>
       </div>
-      <ul>
-        <li>
-          <router-link to="/b/1">Board1</router-link>
-        </li>
-      </ul>
+      <div class="board-item board-item-new">
+        <a class="new-board-btn" href @click.prevent="addBoard">Create new board...</a>
+      </div>
     </div>
   </div>
 </template>
@@ -30,21 +34,74 @@ export default {
 	created() {
 		this.fetchData();
 	},
+	updated() {
+		this.$refs.boardItem.forEach(el => {
+			el.style.backgroundColor = el.dataset.bgcolor;
+		});
+	},
 	methods: {
 		fetchData() {
 			this.loading = true;
 			board
 				.fetch()
 				.then(data => {
-					this.boards = data;
+					this.boards = data.list;
 				})
 				.finally(() => {
 					this.loading = false;
 				});
+		},
+		addBoard() {
+			console.log('addBoard');
 		}
 	}
 };
 </script>
 
 <style scoped>
+.home-title {
+	padding: 10px;
+	font-size: 18px;
+	font-weight: bold;
+}
+.board-list {
+	padding: 10px;
+	display: flex;
+	flex-wrap: wrap;
+}
+.board-item {
+	width: 23%;
+	height: 100px;
+	margin: 0 2% 20px 0;
+	border-radius: 3px;
+}
+.board-item-new {
+	background-color: #ddd;
+}
+.board-item a {
+	text-decoration: none;
+	display: block;
+	width: 100%;
+	height: 100%;
+}
+.board-item a:hover,
+.board-item a:focus {
+	background-color: rgba(0, 0, 0, 0.1);
+	color: #666;
+}
+.board-item-title {
+	color: #fff;
+	font-size: 18px;
+	font-weight: 700;
+	padding: 10px;
+}
+.board-item a.new-board-btn {
+	display: table-cell;
+	vertical-align: middle;
+	text-align: center;
+	height: 100px;
+	width: inherit;
+	color: #888;
+	font-weight: 700;
+}
 </style>
