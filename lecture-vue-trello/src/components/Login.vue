f7712a8cdc4ba000a6a0de83246339fa22e6a453
Seongjoo Jin <evanjin8@gmail.com>
2019-07-22 13:50:41 +0900

    로그인

--- lecture-vue-trello/src/components/Login.vue
+++ lecture-vue-trello/src/components/Login.vue
@@ -1,12 +1,75 @@
 <template>
-  <div>Login</div>
+  <div class="login">
+    <h2>Log in to Trello</h2>
+    <form @submit.prevent="onSubmit">
+      <div>
+        <label for="email">Email</label>
+        <input
+          class="form-control"
+          type="text"
+          name="email"
+          v-model="email"
+          autofocus
+          placeholder="e.g., test@test.com"
+        />
+      </div>
+      <div>
+        <label for="password">Passwrod</label>
+        <input class="form-control" type="password" v-model="password" placeholder="123123" />
+      </div>
+      <button
+        class="btn"
+        :class="{'btn-success': !invalidForm}"
+        type="submit"
+        :disabled="invalidForm"
+      >Log In</button>
+    </form>
+    <p class="error" v-if="error">{{error}}</p>
+  </div>
 </template>
 
 <script>
+import { auth, setAuthInHeader } from '../api';
 export default {
-	name: 'Login'
+	name: 'Login',
+	data() {
+		return {
+			email: '',
+			password: '',
+			returnPath: '',
+			error: ''
+		};
+	},
+	computed: {
+		invalidForm() {
+			return !this.email || !this.password;
+		}
+	},
+	created() {
+		this.returnPath = this.$route.query.rPath || '/';
+	},
+	methods: {
+		onSubmit() {
+			auth.login(this.email, this.password)
+				.then(data => {
+					localStorage.setItem('token', data.accessToken);
+					setAuthInHeader(data.accessToken);
+					this.$router.push(this.returnPath);
+				})
+				.catch(error => {
+					this.error = error.data.error;
+				});
+		}
+	}
 };
 </script>
 
 <style scoped>
+.login {
+	width: 400px;
+	margin: 0 auto;
+}
+.error {
+	color: #f00;
+}
 </style>
