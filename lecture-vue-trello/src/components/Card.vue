8293305a71c117c2a2e3dcf24099c030157f8bc1
evanjin <evanjin8@gmail.com>
2019-07-25 14:34:47 +0900

    카드 상세 조회 1 - 라우팅

--- lecture-vue-trello/src/components/Card.vue
+++ lecture-vue-trello/src/components/Card.vue
@@ -1,12 +1,17 @@
 <template>
-  <div>
+  <Modal>
     Card
     <div v-if="loading">loading card...</div>
     <div v-else>cid: {{ cid }}</div>
-  </div>
+  </Modal>
 </template>
 <script>
+import Modal from './Modal.vue';
+
 export default {
+	components: {
+		Modal
+	},
 	name: 'Card',
 	data() {
 		return {
